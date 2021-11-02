a1452ff2fbf9c795dfe0cd068952a276810ebf91
Yonka <yonka2003@gmail.com>
2021-11-02 20:30:28 +0200

    Добавьте файлы проекта.

---
+++ VisualStudio_filters_Translator/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.IO;
+
+namespace VisualStudio_filters_Translator
+{
+    internal class Program
+    {
+        private static void Main()
+        {
+            string[] files = Directory.GetFiles(Directory.GetCurrentDirectory());
+            foreach (string file in files)
+            {
+                if (Path.GetExtension(file) == ".filters")
+                {
+                    string fileText = File.ReadAllText(file);
+                    fileText = fileText.Replace("Исходные файлы", "Source Files");
+                    fileText = fileText.Replace("Файлы заголовков", "Header Files");
+                    fileText = fileText.Replace("Файлы ресурсов", "Resource Files");
+                    File.WriteAllText(file, fileText);
+                    Console.WriteLine($"[DONE] {file} : Success.");
+
+                    Console.ReadKey();
+                    return;
+                }
+            }
+            Console.WriteLine("[ERROR] Can't find file with '.filters' extesion");
+
+            Console.ReadKey();
+        }
+    }
+}
